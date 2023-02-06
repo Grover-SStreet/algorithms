8e5131aadb2293eb0e974af1d1aefa4700b81658
Grover-SStreet <ajgrover2123@gmail.com>
2023-02-06 12:50:12 -0600

    Completed Stack Question 2 for 25 points

--- assignment1/main.py
+++ assignment1/main.py
@@ -1,103 +1,133 @@
-# python3 program to Implement a stack
-# using singly linked list
-
-class Node:
-
-    # Class to create nodes of linked list
-    # constructor initializes node automatically
-    def __init__(self, data):
-        self.data = data
-        self.next = None
-
-
 class Stack:
+    """
+    Class implementation of a stack utilizing Linked-Lists
+
+    Attributes:
+        front (Node): Head of the stack
+        size (Int): Number of elements in the Stack
+    """
 
     def __init__(self):
-        self.head = None
+        self.front = None
+        self.size = 0
 
-    # Checks if stack is empty
-    def isempty(self):
-        if self.head == None:
-            return True
-        else:
-            return False
+    class Node:
+        """
+        Models a node within the Linked-List / Stack
 
-    # Method to add data to the stack
-    # adds to the start of the stack
+        Attributes:
+            item (Any): Data element of the Node
+            next (Node): Pointer to next Node
+        """
+
+        def __init__(self, item_param):
+            self.item = item_param
+            self.next = None
+
+    def isEmpty(self):
+        """
+        Determines if the stack is empty or not
+
+        Returns:
+            (bool): True or False
+
+        """
+        return self.front is None
+
     def push(self, data):
+        """
+        Pushes an item onto the Stack
 
-        if self.head == None:
-            self.head = Node(data)
+        Args:
+            data: data element to initialize node with
 
+        Returns:
+            None
+        """
+
+        if self.front is None:
+            self.front = self.Node(data)
+            self.size += 1
         else:
-            newnode = Node(data)
-            newnode.next = self.head
-            self.head = newnode
+            self.size += 1
+            temp_node = self.Node(data)
+            temp_node.next = self.front
+            self.front = temp_node
 
-    # Remove element that is the current head (start of the stack)
     def pop(self):
+        """
+        Removes the last in element from the stack
 
-        if self.isempty():
+        Returns:
+            (Node): The node that was popped off the stack
+
+        """
+        if self.isEmpty():
             return None
-
         else:
-            # Removes the head node and makes
-            # the preceding one the new head
-            poppednode = self.head
-            self.head = self.head.next
-            poppednode.next = None
-            return poppednode.data
+            self.size -= 1
+            temp_node = self.front
+            self.front = self.front.next
+            temp_node.next = None
+            return temp_node.item
 
-    # Returns the head node data
-    def peek(self):
+    def __len__(self):
+        """
+        Class implementation of length
 
-        if self.isempty():
-            return None
+        Returns:
+            (int): Size of the stack
 
-        else:
-            return self.head.data
+        """
+        return self.size
 
-    # Prints out the stack
-    def display(self):
+    def __str__(self) -> str:
+        """
+        String implementation of class that prints the Linked-List / Stack contents.
 
-        iternode = self.head
-        if self.isempty():
-            print("Stack Underflow")
+        Returns:
+            output_str (String): A string representation of the Stack
 
-        else:
+        """
+        output_str = "["
+        self.__iter_node = self.front
+        while True:
+            if self.__iter_node is not None:
+                output_str += f'{self.__iter_node.item}'
+                self.__iter_node = self.__iter_node.next
+            if self.__iter_node is not None:
+                output_str += ','
+            else:
+                break
+        output_str += ']'
+        return output_str
 
-            while (iternode != None):
 
-                print(iternode.data, end="")
-                iternode = iternode.next
-                if (iternode != None):
-                    print(" -> ", end="")
-            return
-
-
-# Driver code
 if __name__ == "__main__":
-    MyStack = Stack()
+    # initializing stack
+    stack = Stack()
+    print(f'The stack after initialization is {stack}')
+    assert (stack.isEmpty() == True)  # check if isEmpty works properly
 
-    MyStack.push(11)
-    MyStack.push(22)
-    MyStack.push(33)
-    MyStack.push(44)
+    # add the items to stack
+    for i in range(5):
+        stack.push(i)
+    print(f'The stack after push is {stack}')
 
-    # Display stack elements
-    MyStack.display()
+    assert (stack.isEmpty() == False)  # check if isEmpty works properly
+    assert (len(stack) == 5)  # check if __len__ works properly
 
-    # Print top element of stack
-    print("\nTop element is ", MyStack.peek())
+# pop the stack
+print("Pop the stack")
+while len(stack) > 0:
+    item = stack.pop()
+    print(f'item dequeued from stack: {item}')
+    print(f'stack: {stack}')
+    print(f'length of stack: {len(stack)}')
 
-    # Delete top elements of stack
-    MyStack.pop()
-    MyStack.pop()
+assert (stack.isEmpty() == True)
 
-    # Display stack elements
-    MyStack.display()
-
-    # Print top element of stack
-    print("\nTop element is ", MyStack.peek())
-
-# This code is contributed by Mathew George
+# enqueue the queue
+stack.push(5)
+print(f"\nstack after push: {stack}")
+print(f"length of stack: {len(stack)}")
