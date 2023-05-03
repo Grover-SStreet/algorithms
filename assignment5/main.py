a019f8b8253d6c69980bef539003f783e054f46c
Austin Grover <ajgrover2123@gmail.com>
2023-05-03 16:00:27 -0500

    Finished homeowrk 5

--- assignment5/main.py
+++ assignment5/main.py
@@ -82,46 +82,89 @@ starting_point = 'A'
 
 import heapq
 
-def prim(graph, start):
+import heapq
+import math
+
+import heapq
+
+def dijkstra_with_path(graph, start, end):
     """
     Input:
-        graph: A dictionary representing the adjacency list of the connected, undirected, and weighted graph.
+        graph: A dictionary representing the adjacency list of the connected, directed, and weighted graph.
                The keys are node labels and the values are lists of tuples containing a neighbor and the edge weight.
         start: The starting node label (a key in the graph dictionary).
+        end: The ending node label (a key in the graph dictionary).
 
     Output:
-        Returns the total weight of the minimum spanning tree (MST) of the input graph.
+        Returns the shortest distance between the start and end nodes in the input graph.
     """
 
-    # your code here
-    total_distance = 0
+    # Step 1: Add the starting vertex s to the initially empty fringe with priority value 0
+    # Step 2: Add all other vertices to the fringe with priority value of infinity
 
-    visited = {start}
-    mst_mapping = []
-    priority_queue = []
-    for neighbor, weight in graph[start]:
-        priority_queue.append((weight, (start, neighbor)))
+    previous_node_mapping = {}
 
-    heapq.heapify(priority_queue)
+    distance_mapping = {}
+    for vertex in graph:
+        distance_mapping[vertex] = math.inf
+    distance_mapping[start] = 0
 
-    while len(priority_queue) > 0 or len(visited) != len(graph.keys()):
+    priority_queue = [(0, start)]
+
+    # Step 3: While the fringe is not empty: O(|V|)
+    while len(priority_queue) > 0:
         # Step 3-1: Remove the vertex in the fringe with the minimum priority. O(log |V|)
         current_distance, current_vertex = heapq.heappop(priority_queue)
-        visiting_vertex = current_vertex[1]
-        if visiting_vertex not in visited:
-            for neighbor, weight in graph[visiting_vertex]:
-                if neighbor not in visited:
-                    heapq.heappush(priority_queue, (weight, (visiting_vertex, neighbor)))
 
-            visited.add(visiting_vertex)
-            mst_mapping.append(current_vertex)
-        priority_queue.sort()
+        # Step 3-2: For each of u's neighbors v: O(E_v)
+        for neighbor, weight in graph[current_vertex]:
+            temp_distance = current_distance + weight
 
-    for element in mst_mapping:
-        for index in graph[element[0]]:
-            if index[0] == element[1]:
-                total_distance += index[1]
+            # Update only when path is better
+            if temp_distance < distance_mapping[neighbor]:
+                distance_mapping[neighbor] = temp_distance
+                heapq.heappush(priority_queue, (temp_distance, neighbor))
+                previous_node_mapping[neighbor] = current_vertex
 
-    return total_distance
-print(prim(test_case1,'A'))
-# print(kruskal(test_case2))
+    #  Find from end to start
+    shortest_path = [end]
+
+    while start not in shortest_path:
+        end = previous_node_mapping[end]
+        if start not in shortest_path:
+            shortest_path.append(end)
+
+    shortest_path_distance = 0
+
+    # Reverse List so it shows start to end
+    shortest_path = shortest_path[::-1]
+
+    # From Graph find weights and total them up
+    previous = start
+    for current_vertex in shortest_path:
+        if current_vertex == start:
+            pass
+        else:
+            for neighbor, weight in graph[current_vertex]:
+                if neighbor == previous:
+                    shortest_path_distance += weight
+                    previous = current_vertex
+
+    return shortest_path_distance, shortest_path
+
+test_case4 = {
+    "A": [("B", 2), ("C", 3), ("D", 1)],
+    "B": [("A", 2), ("D", 3), ("E", 5)],
+    "C": [("A", 3), ("D", 4), ("F", 2)],
+    "D": [("A", 1), ("B", 3), ("C", 4), ("E", 1), ("F", 6), ("G", 8)],
+    "E": [("B", 5), ("D", 1), ("G", 6)],
+    "F": [("C", 2), ("D", 6), ("G", 3)],
+    "G": [("D", 8), ("E", 6), ("F", 3)],
+}
+
+
+# Test dijkstra_with_path
+start_node = "A"
+end_node = "G"
+
+distance, path = dijkstra_with_path(test_case4, start_node, end_node)
